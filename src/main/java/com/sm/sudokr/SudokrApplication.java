914712467738468befead66257b257d9c359b789
m-salakhedenov <m.salakhedenov@gmail.com>
2022-10-04 00:37:23 +0600

    Initial commit

---
+++ src/main/java/com/sm/sudokr/SudokrApplication.java
@@ -0,0 +1,13 @@
+package com.sm.sudokr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/sudokr/SudokrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.sudokr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
